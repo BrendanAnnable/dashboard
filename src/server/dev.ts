c9a4d69220aec2eff7d648ab539a7e8df2116af7
Alex Biddulph <c3124185@uon.edu.au>
2020-07-27 20:38:59 +1000

    Use map instead of forEach

--- src/server/dev.ts
+++ src/server/dev.ts
@@ -11,27 +11,25 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackConfig from '../../webpack.config';
 
 import { UDPServers } from './network/network';
-import { TeamData } from './network/network';
 
 // Process command line arguments
 const args = minimist(process.argv.slice(2));
 
 // Get all of the teams from the command line
 // Fall back to our defaults if none were specified
-var teams = args._;
+let teams = args._;
 if (teams.length === 0) {
   teams = ['Red Team:239.226.152.162:9917', 'Blue Team:239.226.152.163:9917'];
 }
 
 // Convert command line data into a more usable format
-var teamData = new Array<TeamData>();
-teams.forEach((team: string) => {
+const teamData = teams.map((team: string) => {
   const nameSplit = team.indexOf(':');
   const portSplit = team.lastIndexOf(':');
   const teamName = team.substring(0, nameSplit);
   const teamAddress = team.substring(nameSplit + 1, portSplit);
   const teamPort = Number(team.substring(portSplit + 1));
-  teamData.push({ name: teamName, address: teamAddress, port: teamPort });
+  return { name: teamName, address: teamAddress, port: teamPort };
 });
 
 const compiler = webpack(webpackConfig);
