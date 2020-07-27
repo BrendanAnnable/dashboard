e49aff57a6ec75d5d56ce42f1a69fec5a50d4757
Alex Biddulph <c3124185@uon.edu.au>
2020-07-27 14:27:25 +1000

    Make sure client is listening to the correct URI

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -5,7 +5,7 @@ import { installDash } from './components/dash/install';
 
 import sio from 'socket.io-client';
 
-const uri = `${document.location!.origin}/dashboard`;
+const uri = `${document.location!.origin}`;
 const sioNetwork = sio(uri, {
   upgrade: false,
   transports: ['websocket'],
@@ -21,7 +21,10 @@ sioNetwork.on('disconnect', () => {
   console.log('Client disconnected from server');
 });
 sioNetwork.on('udp_packet', (...packet: any[]) => {
-  console.log(packet.length);
+  const payload = Buffer.from(packet[0].payload).toString();
+  console.log(
+    `Received a packet from ${packet[0].rinfo.family}:${packet[0].rinfo.address}:${packet[0].rinfo.port} - ${payload}`,
+  );
 });
 sioNetwork.connect();
 
