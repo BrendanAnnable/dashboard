23329ad5857927720d81af3f4feaac8a1e0fb796
Alex Biddulph <c3124185@uon.edu.au>
2020-07-28 20:17:56 +1000

    Change command line to be space separated and add a config file option

--- src/server/prod.ts
+++ src/server/prod.ts
@@ -5,28 +5,54 @@ import http from 'http';
 import minimist from 'minimist';
 import favicon from 'serve-favicon';
 import sio from 'socket.io';
+import fs from 'fs';
+import yaml from 'yaml';
 
 import { UDPServers } from './network/network';
+import { TeamData } from './network/network';
 
-// Process command line arguments
-const args = minimist(process.argv.slice(2));
+// If the config file exists read it in instead of reading from the command line
+let teamData: TeamData[] = [];
+const stats = fs.statSync('config.yaml');
+if (!stats.isFile()) {
+  console.log(
+    'Could not find config.yaml. Defaulting to command line team specification',
+  );
+  // Process command line arguments
+  const args = minimist(process.argv.slice(2));
 
-// Get all of the teams from the command line
-// Fall back to our defaults if none were specified
-let teams = args._;
-if (teams.length === 0) {
-  teams = ['Red Team:239.226.152.162:9917', 'Blue Team:239.226.152.163:9917'];
+  // Get all of the teams from the command line
+  // Fall back to our defaults if none were specified
+  // The list of arguments should be a multiple of 3
+  // <team name> <multicast address> <port> <team name> <multicast address> <port>
+  let teams = args._;
+  if (teams.length === 0 || teams.length % 3 !== 0) {
+    teams = [
+      'Red Team',
+      '239.226.152.162',
+      '9917',
+      'Blue Team',
+      '239.226.152.163',
+      '9917',
+    ];
+  }
+
+  // Convert command line data into a more usable format
+  // Command line data is expected to be a space-separated list of team data
+  // <team name> <multicast address> <port> <team name> <multicast address> <port>
+  for (let i = 0; i <= teams.length - 3; i += 3) {
+    teamData.push({
+      name: teams[i],
+      address: teams[i + 1],
+      port: Number(teams[i + 2]),
+    });
+  }
+} else {
+  console.log('Using config.yaml for team specification');
+  teamData = yaml.parse(
+    fs.readFileSync('config.yaml', { encoding: 'utf8', flag: 'r' }),
+  )['teams'];
 }
-
-// Convert command line data into a more usable format
-const teamData = teams.map((team: string) => {
-  const nameSplit = team.indexOf(':');
-  const portSplit = team.lastIndexOf(':');
-  const teamName = team.substring(0, nameSplit);
-  const teamAddress = team.substring(nameSplit + 1, portSplit);
-  const teamPort = Number(team.substring(portSplit + 1));
-  return { name: teamName, address: teamAddress, port: teamPort };
-});
 
 const app = express();
 const server = http.createServer(app);
